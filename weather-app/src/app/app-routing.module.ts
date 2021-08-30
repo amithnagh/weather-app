c880757d75104b4b312454234e342b8393f142dd
amith <amithnagh.p@gmail.com>
2021-08-30 17:29:21 +0530

    Created basic structure

--- weather-app/src/app/app-routing.module.ts
+++ weather-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherDetailComponent } from './views/weather-detail/weather-detail.component';
+import { WeatherHomeComponent } from './views/weather-home/weather-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'home',
+  component: WeatherHomeComponent,
+  },
+  { path: 'detail',
+    component: WeatherDetailComponent  
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherDetailComponent } from './views/weather-detail/weather-detail.component';
+import { WeatherHomeComponent } from './views/weather-home/weather-home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherDetailComponent,
+    WeatherHomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
